ae596b1d76af20f98706ea80c690074368189596
bharathram.manoharan <bharathram.manoharan@salesforce.com>
2020-04-01 23:55:10 -0700

    selenium test code

---
+++ src/test/java/TestNG/verifyaboutme.java
@@ -0,0 +1,53 @@
+package TestNG;
+
+//import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class verifyaboutme {
+	
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void launch() {
+		
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+		driver = new ChromeDriver();
+		
+		driver.get("http://35.223.124.145");
+		
+	}
+	
+  @Test
+  public void verifyabout() {
+	    
+	    driver.findElement(By.id("About Us")).click();
+
+	    String text ;
+	    
+		text = "This is about page";
+		
+		String bodyText = driver.findElement(By.tagName("body")).getText();
+		
+		//System.out.println(bodyText);		
+		
+		Assert.assertTrue(bodyText.contains(text));
+		
+   }
+  
+   @AfterMethod
+   public void close() {
+	   
+	   driver.quit();
+   
+   }
+  
+  
+}
